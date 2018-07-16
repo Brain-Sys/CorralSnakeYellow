46dd7691968637f880748ff0f61366cce803ff1c
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-07-16 16:56:44 +0200

    -

---
+++ Aula5.CorralSnakeYellow.DomainModel/IdGeneratorEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula5.CorralSnakeYellow.DomainModel
+{
+    class IdGeneratorEngine
+    {
+        public int GetNextIdForCliente()
+        {
+            return 0;
+        }
+
+        public int GetNextIdForCartaCredito()
+        {
+            return 7;
+        }
+    }
+}
--- Aula5.CorralSnakeYellow.ScheduledTask/Program.cs
+++ Aula5.CorralSnakeYellow.ScheduledTask/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Aula5.CorralSnakeYellow.DomainModel;
+using System;
 
 namespace Aula5.CorralSnakeYellow.ScheduledTask
 {
@@ -10,6 +7,31 @@ namespace Aula5.CorralSnakeYellow.ScheduledTask
     {
         static void Main(string[] args)
         {
+            ContoCorrente conto = new ContoCorrente();
+
+            if (DateTime.Now.Day == 16)
+            {
+                conto = new ContoCorrente();
+                conto.Cliente = null;
+            }
+            else
+            {
+                // conto = leggereDaFileCsv();
+            }
+
+            conto.DataApertura = DateTime.Now;
+
+            Cliente io = new Cliente();
+            io.Cognome = "Rossi";
+            io.Nome = "Mario";
+            io.DataNascita = new DateTime(1976, 2, 28);
+
+            var cc = new CartaCredito();
+            var cd = new CartaDebito().DataAttivazione.Month == 2;
+
+
+            cc = null;
+            cc.LimiteMensile = 560;
         }
     }
 }
