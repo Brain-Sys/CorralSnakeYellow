a2808af662d723a3284e971437b022f60350b6fe
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-07-18 12:30:00 +0200

    -

---
+++ Aula5.CorralSnakeYellow.UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aula5.CorralSnakeYellow.UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aula5.CorralSnakeYellow.UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e37f5ee-5d25-48fe-b305-24e8a5f8681f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Aula5.CorralSnakeYellow.UnitTesting/Testing.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Aula5.CorralSnakeYellow.BusinessLayer;
+using Aula5.CorralSnakeYellow.DomainModel;
+using Aula5.CorralSnakeYellow.Repository.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aula5.CorralSnakeYellow.UnitTesting
+{
+    // Batteria di test
+    [TestClass]
+    public class Testing
+    {
+        // Metodo di test
+        [TestMethod]
+        public void CreazioneClienteConCognomeVuoto()
+        {
+            Cliente cliente = Cliente.Factory.Crea();
+            cliente.Cognome = "";
+
+            Assert.IsNotNull(cliente);
+        }
+
+        // Metodo di test
+        [TestMethod]
+        public void CreazioneClienteConDataNascitaDefault()
+        {
+            Cliente cliente = Cliente.Factory.Crea();
+
+            // 01/01/0001
+            Assert.AreNotEqual(DateTime.MinValue, cliente.DataNascita);
+        }
+
+        [TestMethod]
+        public void TestMinimaleSulDatabase()
+        {
+            IRepo repo = DependencyResolver.Get();
+
+            var x = repo.Clienti.ToList().Count;
+            Assert.AreEqual(0, x);
+            //Assert.IsTrue(x == 0);
+        }
+    }
+}
